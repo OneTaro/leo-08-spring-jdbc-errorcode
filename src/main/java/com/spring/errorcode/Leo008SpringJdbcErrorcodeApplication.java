71af64ae7a32aa8e11e6f6befd3f4bd922b51a27
李白与孟浩然 <OneTaro@users.noreply.github.com>
2021-05-21 21:15:23 +0800

    spring项目

--- src/main/java/com/spring/errorcode/Leo008SpringJdbcErrorcodeApplication.java
+++ src/main/java/com/spring/errorcode/Leo008SpringJdbcErrorcodeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Leo08SpringJdbcErrorcodeApplication {
+public class Leo008SpringJdbcErrorcodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Leo08SpringJdbcErrorcodeApplication.class, args);
+        SpringApplication.run(Leo008SpringJdbcErrorcodeApplication.class, args);
     }
 
 }
